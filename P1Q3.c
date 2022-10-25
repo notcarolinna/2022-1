acde388c3606ae77cbbd44ce6ee41fcb10b18fdd
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-10-25 12:04:17 -0300

    Update P1Q3.c

--- P1Q3.c
+++ P1Q3.c
@@ -12,17 +12,3 @@ int main() {
 
   printf("O resultado da potência é: %d!", potencia);
 }
-#include <math.h>  //para poder usar o pow
-#include <stdio.h>
-
-int main() {
-  // Escreva um programa que recebe dois inteiros a e b e calcula a^b.
-
-  int base, expoente, potencia;
-  printf("Digite o valor da base e do expoente:\n");
-  scanf("%d%d", &base, &expoente);
-
-  potencia = pow(base, expoente);
-
-  printf("O resultado da potência é: %d!", potencia);
-}
